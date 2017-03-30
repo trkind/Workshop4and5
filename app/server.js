d015ef9a0b00342c4110112b64e1535353c7a7c6
Trent Kindvall <tgkindvall@umass.edu>
2017-03-30 16:18:53 -0400

    fb4

--- app/server.js
+++ app/server.js
@@ -1,5 +1,97 @@
 import {readDocument, writeDocument, addDocument} from './database.js';
+import { getFeedData } from './server';
+import { getFeedData, createComment } from './server';
 
+var initialData = {
+  // The "user" collection. Contains all of the users in our Facebook system.
+  "users": {
+    // This user has id "1".
+    "1": {
+      "_id": 1,
+      "fullName": "Someone",
+      "feed": 1
+    },
+    "2": {
+      "_id": 2,
+      "fullName": "Someone Else",
+      "feed": 2
+    },
+    "3": {
+      "_id": 3,
+      "fullName": "Another Person",
+      "feed": 3
+    },
+    // This is "you"!
+    "4": {
+      "_id": 4,
+      "fullName": "John Vilk",
+      // ID of your feed.
+      "feed": 4
+    }
+  },
+  // The 'feedItems' collection. Contains all of the feed items on our Facebook
+  // system.
+  "feedItems": {
+    "1": {
+      "_id": 1,
+      // A list of users that liked the post. Here, "Someone Else" and "Another Person"
+      // liked this particular post.
+      "likeCounter": [
+        2, 3
+      ],
+      // The type and contents of this feed item. This item happens to be a status
+      // update.
+      "type": "statusUpdate",
+      "contents": {
+        // ID of the user that posted the status update.
+        "author": 1,
+        // 01/24/16 3:48PM EST, converted to Unix Time
+        // (# of milliseconds since Jan 1 1970 UTC)
+        // https://en.wikipedia.org/wiki/Unix_time
+        "postDate": 1453668480000,
+        "location": "Austin, TX",
+        "contents": "ugh."
+      },
+      // List of comments on the post
+      "comments": [
+        {
+          // The author of the comment.
+          "author": 2,
+          // The contents of the comment.
+          "contents": "hope everything is ok!",
+          // The date the comment was posted.
+          // 01/24/16 22:00 EST
+          "postDate": 1453690800000
+        },
+        {
+          "author": 3,
+          "contents": "sending hugs your way",
+          "postDate": 1453690800000
+        }
+      ]
+    }
+  },
+  // "feeds" collection. Feeds for each FB user.
+  "feeds": {
+    "4": {
+      "_id": 4,
+      // Listing of FeedItems in the feed.
+      "contents": [1]
+    },
+    "3": {
+      "_id": 3,
+      "contents": []
+    },
+    "2": {
+      "_id": 2,
+      "contents": []
+    },
+    "1": {
+      "_id": 1,
+      "contents": []
+    }
+  }
+};
 /**
  * Emulates how a REST call is *asynchronous* -- it calls your function back
  * some time in the future with data.
@@ -8,4 +100,47 @@ function emulateServerReturn(data, cb) {
   setTimeout(() => {
     cb(data);
   }, 4);
+
+
+}
+
+/**
+ * Given a feed item ID, returns a FeedItem object with references resolved.
+ * Internal to the server, since it's synchronous.
+ */
+function getFeedItemSync(feedItemId) {
+  var feedItem = readDocument('feedItems', feedItemId);
+  // Resolve 'like' counter.
+  feedItem.likeCounter =
+    feedItem.likeCounter.map((id) => readDocument('users', id));
+  // Assuming a StatusUpdate. If we had other types of
+  // FeedItems in the DB, we would
+  // need to check the type and have logic for each type.
+  feedItem.contents.author =
+    readDocument('users', feedItem.contents.author);
+  // Resolve comment author.
+  feedItem.comments.forEach((comment) => {
+    comment.author = readDocument('users', comment.author);
+  });
+  return feedItem;
+}
+
+/**
+ * Emulates a REST call to get the feed data for a particular user.
+ * @param user The ID of the user whose feed we are requesting.
+ * @param cb A Function object, which we will invoke when the Feed's data is available.
+ */
+export function getFeedData(user, cb) {
+  // Get the User object with the id "user".
+  var userData = readDocument('users', user);
+  // Get the Feed object for the user.
+  var feedData = readDocument('feeds', userData.feed);
+  // Map the Feed's FeedItem references to actual FeedItem objects.
+  // Note: While map takes a callback function as an argument, it is
+  // synchronous, not asynchronous. It calls the callback immediately.
+  feedData.contents = feedData.contents.map(getFeedItemSync);
+  // Return FeedData with resolved references.
+  // emulateServerReturn will emulate an asynchronous server operation, which
+  // invokes (calls) the "cb" function some time in the future.
+  emulateServerReturn(feedData, cb);
 }
